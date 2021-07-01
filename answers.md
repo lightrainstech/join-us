0a65dbabe7db1761fbbed93303139b6446d08d3b
dibinjose <dibinantonyjose@gmail.com>
2021-07-01 11:58:14 +0530

    4th question finished

--- answers.md
+++ answers.md
@@ -51,3 +51,31 @@ console.log(result)
 ```
 
 4.
+```js
+// Function to convert text into pigLatin
+const toPigLatin = (text) => {
+    const wordsArray = text.split(" ")
+    const pigLatinArray = wordsArray.map(element => {
+        let firstLetter = element[0];
+        let newElement = element.slice(1)
+        newElement = newElement + firstLetter + 'ay'
+        return newElement
+    })
+    return pigLatinArray.join(" ")
+}
+
+const toText = (text) => {
+    const pigLatinArray = text.split(" ")
+    const wordsArray = pigLatinArray.map(element => {
+        let firstLetter = element[element.length-3];
+        let newElement = element.slice(0, element.length-3)
+        newElement = firstLetter + newElement
+        return newElement
+    })
+    return wordsArray.join(" ")
+}
+
+
+console.log(toPigLatin("Hii Dibin Jose"))
+console.log(toText("iiHay ibinDay oseJay"))
+```
--- sample.js
+++ sample.js
@@ -1,10 +1,26 @@
+// Function to convert text into pigLatin
 const toPigLatin = (text) => {
-    const words = text.split(" ")
-    console.log(words)
+    const wordsArray = text.split(" ")
+    const pigLatinArray = wordsArray.map(element => {
+        let firstLetter = element[0];
+        let newElement = element.slice(1)
+        newElement = newElement + firstLetter + 'ay'
+        return newElement
+    })
+    return pigLatinArray.join(" ")
 }
 
 const toText = (text) => {
-    
+    const pigLatinArray = text.split(" ")
+    const wordsArray = pigLatinArray.map(element => {
+        let firstLetter = element[element.length-3];
+        let newElement = element.slice(0, element.length-3)
+        newElement = firstLetter + newElement
+        return newElement
+    })
+    return wordsArray.join(" ")
 }
 
-const pig
+
+console.log(toPigLatin("Hii Dibin Jose"))
+console.log(toText("iiHay ibinDay oseJay"))
