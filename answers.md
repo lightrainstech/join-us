a52d84a83dac9fad1816da0e9c58e8136e468995
dibinjose <dibinantonyjose@gmail.com>
2021-07-01 12:21:56 +0530

    5th question finished

--- answers.md
+++ answers.md
@@ -79,3 +79,18 @@ const toText = (text) => {
 console.log(toPigLatin("Hii Dibin Jose"))
 console.log(toText("iiHay ibinDay oseJay"))
 ```
+
+5.
+```js
+const rotateElements = (arr, k) => {
+    // let newArray = [];
+    for(let i=0; i<k; i++) {
+        arr.push(arr.shift())
+    }
+        return arr;
+}
+
+const sampleArray = [1,2,3,4,5,6,7,8,9]
+const k = 2
+console.log(rotateElements(sampleArray, k))
+```
--- sample.js
+++ sample.js
@@ -1,26 +1,11 @@
-// Function to convert text into pigLatin
-const toPigLatin = (text) => {
-    const wordsArray = text.split(" ")
-    const pigLatinArray = wordsArray.map(element => {
-        let firstLetter = element[0];
-        let newElement = element.slice(1)
-        newElement = newElement + firstLetter + 'ay'
-        return newElement
-    })
-    return pigLatinArray.join(" ")
+const rotateElements = (arr, k) => {
+    // let newArray = [];
+    for(let i=0; i<k; i++) {
+        arr.push(arr.shift())
+    }
+        return arr;
 }
 
-const toText = (text) => {
-    const pigLatinArray = text.split(" ")
-    const wordsArray = pigLatinArray.map(element => {
-        let firstLetter = element[element.length-3];
-        let newElement = element.slice(0, element.length-3)
-        newElement = firstLetter + newElement
-        return newElement
-    })
-    return wordsArray.join(" ")
-}
-
-
-console.log(toPigLatin("Hii Dibin Jose"))
-console.log(toText("iiHay ibinDay oseJay"))
+const sampleArray = [1,2,3,4,5,6,7,8,9]
+const k = 2
+console.log(rotateElements(sampleArray, k))
